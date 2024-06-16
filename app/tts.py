c77757dc01880b4142ff2b84f50ab541c7adb989
dakShh <daksh.dk207@gmail.com>
2024-06-15 21:32:26 -0400

    implemented eleven labs api

--- app/tts.py
+++ app/tts.py
@@ -1,11 +1,26 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from elevenlabs import VoiceSettings
+from elevenlabs.client import ElevenLabs
 from TTS.utils.manage import ModelManager
 from TTS.utils.synthesizer import Synthesizer
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 import site
 location = site.getsitepackages()[0]
 
-path = location+"/TTS/.models.json"
-# path = "C:/Users/asus/anaconda3/envs/texttospeech/Lib/site-packages/TTS/.models.json"
+# path = location+"/TTS/.models.json"
+path = "C:/Users/asus/anaconda3/envs/texttospeech/Lib/site-packages/TTS/.models.json"
+
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+print("ELEVENLABS_API_KEY: ",ELEVENLABS_API_KEY)
+client = ElevenLabs(
+    api_key=ELEVENLABS_API_KEY,
+)
+
 model_manager = ModelManager(path)
 
 model_path, config_path, model_item = model_manager.download_model("tts_models/en/ljspeech/tacotron2-DDC")
@@ -18,5 +33,4 @@ syn = Synthesizer(
     vocoder_checkpoint=voc_path,
     vocoder_config=voc_config_path
 )
-
 
--- app/views.py
+++ app/views.py
@@ -1,10 +1,30 @@
 from distutils.log import error
 import mimetypes
 from re import template
+
+from elevenlabs import VoiceSettings
 from app import app
 from flask import render_template, request, send_file
 from app.tts import syn
+
+
+from dotenv import load_dotenv
+from elevenlabs import VoiceSettings
+from elevenlabs.client import ElevenLabs
+
+import os
 import io
+
+load_dotenv()
+
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+
+if not ELEVENLABS_API_KEY:
+    raise ValueError("ELEVENLABS_API_KEY environment variable not set")
+
+client = ElevenLabs(
+    api_key=ELEVENLABS_API_KEY,
+)
 
 @app.route("/")
 def index(): 
@@ -18,5 +38,47 @@ def call_project1():
         out = io.BytesIO()
         syn.save_wav(outputs, out)
         return send_file(out, mimetype="audio/wav")
+    else:
+        return "Please enter text! :)", 400
+    
+
+@app.route("/call/voicify", methods = ["POST"])
+def call_voicify(): 
+    if (request.form['text']):
+        text = request.form['text']
+        # outputs = syn.tts(text)
+        # out = io.BytesIO()
+        # syn.save_wav(outputs, out)
+        # return send_file(out, mimetype="audio/wav")
+        # Perform the text-to-speech conversion
+        response = client.text_to_speech.convert(
+            voice_id="pNInz6obpgDQGcFmaJgB",  # Adam pre-made voice
+            optimize_streaming_latency="0",
+            output_format="mp3_22050_32",
+            text=text,
+            model_id="eleven_multilingual_v2",
+            voice_settings=VoiceSettings(
+                stability=0.0,
+                similarity_boost=1.0,
+                style=0.0,
+                use_speaker_boost=True,
+            ),
+        )
+
+        print("Streaming audio data...")
+
+        # Create a BytesIO object to hold audio data
+        audio_stream = io.BytesIO()
+
+        # Write each chunk of audio data to the stream
+        for chunk in response:
+            if chunk:
+                audio_stream.write(chunk)
+
+        # Reset stream position to the beginning
+        audio_stream.seek(0)
+        return audio_stream
+
+
     else:
         return "Please enter text! :)", 400
